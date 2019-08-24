db14ab7fecca47edf878254b06ef0c8e0ebe8aae
Java Adore <java.adore@gmail.com>
2019-08-24 03:55:07 +0200

    implementation of first simple greetings actor

---
+++ src/main/scala/GreetingActor.scala
@@ -0,0 +1,26 @@
+import akka.actor.{Actor, ActorSystem, Props}
+
+case class Greeting(name:String)
+
+class GreetingActor extends Actor{
+
+
+  override def receive: Receive = {
+
+    case Greeting(name) => println(s"welcome ya $name")
+    case _ => println("unknown message type")
+  }
+
+}
+
+
+object Home extends App{
+
+  val system = ActorSystem("greeter")
+
+ val greetingsActorRef= system.actorOf(Props[GreetingActor])
+
+  greetingsActorRef ! Greeting("Mahmoud")
+
+
+}
